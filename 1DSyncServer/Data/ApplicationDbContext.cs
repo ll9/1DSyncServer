fc34290efa1790ad126f904e245044b4443b2a2a
ll9 <llwierer@web.de>
2019-02-17 20:51:41 +0100

    db context

---
+++ 1DSyncServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using _1DSync.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1DSyncServer.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<PseudoDynamicEntity> PseudoDynamicEntities { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
